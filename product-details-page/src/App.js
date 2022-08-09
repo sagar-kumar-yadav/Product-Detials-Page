7e436717845c45354e6c3b626fc6b8f4d668583d
sagar-kumar-yadav <sk5323912@gmail.com>
2022-08-10 02:22:12 +0530

    new commit

--- product-details-page/src/App.js
+++ product-details-page/src/App.js
@@ -1,56 +1,32 @@
+import React, { Component } from 'react';
+
 import classes from './App.module.css';
 import ProductData from './ProductData';
+import ProductDetails from './ProductDetails';
+import ProductPreview from './ProductPreview';
+import Topbar from './Topbar';
 
-function App() {
-  const currentHour = new Date().getHours() > 9 ? new Date().getHours() : '0' + new Date().getHours();
-  const currentMinutes =  new Date().getMinutes() > 9 ? new Date().getMinutes() : '0' + new Date().getMinutes();
-  return (
-    <div className="App">
-      <header className='app-header'>
-        <nav className={classes.Topbar}>
-          <img src='https://pngimg.com/uploads/amazon/amazon_PNG11.png' alt='image-link'/>
-        </nav>
-      </header>
+class App extends Component {
+  state = {
+    productData: ProductData
+  }
 
-      <div className={classes.MainContainer}>
-        <div className={classes.ProductPreview}>
-          <img src='https://imgur.com/iOeUBV7.png' alt='Product Preview'/>
-
-          {/* <div className={classes.TimeSection}>
-            <p>{`${currentHour}:${currentMinutes}`}</p>
-          </div> */}
-
-          <div className={classes.HeartBeatSection}>
-            <i class="fas fa-heartbeat"></i>
-            <p>78</p>
+  render() {
+    return (
+      <div className="App">
+          <Topbar />
+        <div className={classes.MainContainer}>
+          <div className={classes.ProductPreview}>
+            <ProductPreview />
           </div>
-        </div>
-
-        <div className={classes.ProductData}>
-          <h1 className={classes.ProductTitle}>{ProductData.title}</h1>
-          <p className={classes.ProductDescription}>{ProductData.description}</p>
-
-          <h3 className={classes.SectionHeading}>Select Color</h3>
-          <div>
-            <img className={[classes.ProductImage, classes.SelectedProductImage].join(' ')} src='https://imgur.com/iOeUBV7.png' alt='Black Color Watch'/>
-            <img className={classes.ProductImage} src='https://imgur.com/PTgQlim.png' alt='Red Color Watch'/>
-            <img className={classes.ProductImage} src='https://imgur.com/Mplj1YR.png' alt='Blue Color Watch'/>
-            <img className={classes.ProductImage} src='https://imgur.com/xSIK4M8.png' alt='Purple Color Watch'/>
-          </div>
-
-          <div>
-            <h3 className={classes.SectionHeading}>Features</h3>
-            <div>
-              <button className={[classes.FeatureItem, classes.SelectedFeatureItem].join(' ')}>Time</button>
-              <button className={classes.FeatureItem}>Heart Rate</button>
-            </div>
-
-            <button className={classes.PrimaryButton}>Buy Now</button>
+  
+          <div className={classes.ProductData}>
+            <ProductDetails data={this.state.productData} />
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
